4332b29fc50f4a698aa7baa245d5ee023d525418
hermesonbastos <hermesonbastos@gmail.com>
2024-02-25 20:22:19 -0300

    ler argumentos

---
+++ 02_fundamentos/04_ler_argumentos/index.js
@@ -0,0 +1,14 @@
+// script para resgatar o nome
+
+// console.log(process.argv);
+
+const args = process.argv.slice(2);
+console.log(args)
+
+const nome = args[0].split("=")[1];
+// console.log(nome);
+
+const idade = args[1].split("=")[1];
+// console.log(idade);
+
+console.log(`Oi me chamo ${nome} e tenho ${idade} anos`)
