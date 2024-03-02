7a10827eabac0ea1f1872987f71623b7a79b2976
hermesonbastos <hermesonbastos@gmail.com>
2024-03-02 00:42:04 -0300

    atualização de teste

--- 03_core_modules/02_retornando_html/index.js
+++ 03_core_modules/02_retornando_html/index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   
   res.statusCode = 200;
   res.setHeader('Contenty-type', 'text/html');
-  res.end("<h1>Olá esse é meu primeiro server que retorna HTML!</h1>");
+  res.end("<h1>Olá esse é meu primeiro server que retorna HTML!</h1><p>Testando Atualização</p>");
 })
 
 server.listen(port, () => {
