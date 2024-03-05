5a7397a3b7fec750b93c2cc0710509f32422b309
hermesonbastos <hermesonbastos@gmail.com>
2024-03-05 16:39:50 -0300

    escrevendo arquivos

--- 03_core_modules/06_escrevendo_arquivos/index.js
+++ 03_core_modules/06_escrevendo_arquivos/index.js
@@ -6,16 +6,22 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 
   const urlInfo = require("url").parse(req.url, true);
-  const name = urlInfo.query.name;
+  let name = urlInfo.query.name;
 
-  fs.readFile("index.html ", (err, data) => {
-
-    if(!name) {
-      
-    }
-
-  })
-
+  if(!name) {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    })
+  } else {
+    fs.writeFile("arquivo.txt", name, function(err, data) {
+      res.writeHead(301, {
+        Location: '/',
+      })
+      return res.end();
+    })
+  }
 });
 
 server.listen(port, () => {
