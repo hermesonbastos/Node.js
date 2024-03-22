96ea23e9143cd14e1ef757a3a8d5eb70f42e1a5c
hermesonbastos <hermesonbastos@gmail.com>
2024-03-22 10:26:56 -0300

    os core module

---
+++ 03_core_modules/15_os/index.js
@@ -0,0 +1,8 @@
+const os = require("os");
+
+console.log(os.freemem());
+console.log(os.cpus());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.type());
+console.log(os.machine());
