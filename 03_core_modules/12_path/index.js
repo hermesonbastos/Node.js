c2fa49059d90b0f5a490ef0d04381c41c152c416
hermesonbastos <hermesonbastos@gmail.com>
2024-03-22 09:36:06 -0300

    path

---
+++ 03_core_modules/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const caminho = "/curriculos/Hermeson/curriculo.pdf";
+
+console.log(path.dirname(caminho));
+console.log(path.basename(caminho));
+console.log(path.extname(caminho));
